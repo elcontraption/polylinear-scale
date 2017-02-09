6a2242fb44ee63797b6020a5d00252eb6f1b2c27
Darin Reid <darinreid@gmail.com>
2017-02-09 09:26:55 -0800

    Wrapping returned function

--- dist/polylinear-scale.js
+++ dist/polylinear-scale.js
@@ -4,57 +4,6 @@
 	(factory());
 }(this, (function () { 'use strict';
 
-var config = {};
-
-/**
- * The compiled scale to return
- *
- * @param  {Number} value The number to scale
- * @return {Number}       The result
- */
-function scale(value) {
-  var domains = config.domain;
-  var ranges = config.range;
-  var rangeMin;
-  var rangeMax;
-  var domain;
-  var range;
-  var ratio;
-  var result;
-  var i = 0;
-
-  /* eslint-disable no-sequences */
-  while (i < domains.length - 1) {
-    if (value >= domains[i] && value <= domains[i + 1]) {
-      domain = [domains[i], domains[i + 1]], range = [ranges[i], ranges[i + 1]];
-      break;
-    }
-    i++;
-  }
-
-  // Value is outside given domain
-  if (domain === undefined) {
-    if (value < domains[0]) {
-      domain = [domains[0], domains[1]];
-      range = [ranges[0], ranges[1]];
-    } else {
-      domain = [domains[domains.length - 2], domains[domains.length - 1]];
-      range = [ranges[ranges.length - 2], ranges[ranges.length - 1]];
-    }
-  }
-
-  ratio = (range[1] - range[0]) / (domain[1] - domain[0]);
-  result = range[0] + ratio * (value - domain[0]);
-
-  if (config.clamp) {
-    rangeMin = Math.min(range[0], range[1]);
-    rangeMax = Math.max(range[0], range[1]);
-    result = Math.min(rangeMax, Math.max(rangeMin, result));
-  }
-
-  return result;
-}
-
 /**
  * A polylinear scale
  *
@@ -66,19 +15,60 @@ function scale(value) {
  * @return {Function}         Scale function
  */
 function polylinearScale(domain, range, clamp) {
-  domain = domain || [0, 1];
-  range = range || [0, 1];
+  var domains = domain || [0, 1];
+  var ranges = range || [0, 1];
   clamp = clamp || false;
 
-  if (domain.length !== range.length) {
+  if (domains.length !== ranges.length) {
     throw new Error('polylinearScale requires domain and range to have an equivalent number of values');
   }
 
-  config.domain = domain;
-  config.range = range;
-  config.clamp = clamp;
+  /**
+   * The compiled scale to return
+   *
+   * @param  {Number} value The number to scale
+   * @return {Number}       The result
+   */
+  return function (value) {
+    var rangeMin = void 0;
+    var rangeMax = void 0;
+    var domain = void 0;
+    var range = void 0;
+    var ratio = void 0;
+    var result = void 0;
+    var i = 0;
 
-  return scale;
+    /* eslint-disable no-sequences */
+    while (i < domains.length - 1) {
+      if (value >= domains[i] && value <= domains[i + 1]) {
+        domain = [domains[i], domains[i + 1]], range = [ranges[i], ranges[i + 1]];
+        break;
+      }
+      i++;
+    }
+
+    // Value is outside given domain
+    if (domain === undefined) {
+      if (value < domains[0]) {
+        domain = [domains[0], domains[1]];
+        range = [ranges[0], ranges[1]];
+      } else {
+        domain = [domains[domains.length - 2], domains[domains.length - 1]];
+        range = [ranges[ranges.length - 2], ranges[ranges.length - 1]];
+      }
+    }
+
+    ratio = (range[1] - range[0]) / (domain[1] - domain[0]);
+    result = range[0] + ratio * (value - domain[0]);
+
+    if (clamp) {
+      rangeMin = Math.min(range[0], range[1]);
+      rangeMax = Math.max(range[0], range[1]);
+      result = Math.min(rangeMax, Math.max(rangeMin, result));
+    }
+
+    return result;
+  };
 }
 
 module.exports = polylinearScale;
